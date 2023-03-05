5a54eb631f0f2dc5b63bc3d4fd37d920f30c978d
BBC <bedabrata.chatterjee@gmail.com>
2023-03-05 15:27:58 +0530

    Added example of mutable references

--- README.md
+++ README.md
@@ -1,51 +1,22 @@
 # how_to_rust-
 How to Rust?
 
-
-## Hello World!
-
-
-```bash
-
-# Create the program
-$ cargo new helloworld 
-     Created binary (application) `helloworld` package
-
-# build and run the cargo package
-$ cargo run
-
-   Compiling helloworld v0.1.0 (/home/bbc/progs/rust/how_to_rust/helloworld)
-    Finished dev [unoptimized + debuginfo] target(s) in 0.33s
-     Running `target/debug/helloworld`
-Hello, world!
-
-# The code would be created in target/debug folder
-$ ./target/debug/helloworld 
-Hello, world!
-
-
-
-# To build the code in the release mode
-$ cargo run --release      
-   Compiling helloworld v0.1.0 (/home/bbc/progs/rust/how_to_rust/helloworld)
-    Finished release [optimized] target(s) in 0.16s
-     Running `target/release/helloworld`
-Hello, world!
-
-
-```
-
------------
-
+Follow [this document](https://www.rust-lang.org/learn/get-started) to get started with rust. 
+--------------------
 ## List of exercises
+
+Let's step into the world of Rust. We have lots of things to cover so here is a step-wise exploration of the key topics.
 
 | Sequence | Topic  | Description |
 | ---------| -----  | ----------- |
-| 1 | [HelloWorld!](./code/helloworld/src/main.rs) | Getting started with rust |
-| 2 | [Variables and DataTypes](./code/variables/src/main.rs) | Exploring datatypes in rust |
-| 3 | [Custom libraries](./code/demo_package/src/main.rs) | How to create and use libraries in rust |
-| 4 | [Control flow](./code/control_flow/src/main.rs) | Exploring control flows in rust |
-| 5 | [Strings](./code/demo_strings/src/main.rs) | Exploring Strings in rust |
+| 1 | [HelloWorld!](./code/helloworld/src/main.rs) | Getting started with Rust |
+| 2 | [Variables and DataTypes](./code/variables/src/main.rs) | Exploring datatypes|
+| 3 | [Custom libraries](./code/demo_package/src/main.rs) | How to create and use libraries|
+| 4 | [Control flow](./code/control_flow/src/main.rs) | Exploring control flows|
+| 5 | [Strings](./code/demo_strings/src/main.rs) | Exploring Strings, Ownerships, References and multable references |
 
 ------------
 
+## References
+
+* [Ultimate-Rust-Crash-Course by Nathan Stocks](https://www.udemy.com/course/ultimate-rust-crash-course/)
--- code/demo_strings/src/main.rs
+++ code/demo_strings/src/main.rs
@@ -3,6 +3,8 @@ fn main() {
     read_commandline();
 
     string_ownership();
+
+    string_ownership2();
 }
 
 fn read_commandline() {
@@ -18,7 +20,7 @@ fn string_ownership(){
     let s1 = String::from("abc");
     println!("s1: {}", s1);
 
-    // there is no copy, the value of s1 has been moved to s2
+    // there is no copy, the value of s1 has been MOVED to s2
     let s2 = s1;
 
     println!("s2: {}", s2);
@@ -27,4 +29,64 @@ fn string_ownership(){
     // this statement will throw an error
     // println!("s1: {}", s1);
 
+    // we can clone the value of s2 to s3
+    // In rust, we use the term copy if only stack data is copied
+    // if heap data is copied or pointer manipulation is involved,  we use the term clone
+    let s3 = s2.clone();
+    println!("s3: {}", s3);
+
+}
+
+fn string_ownership2(){
+    let s1 = String::from("abc");
+    let mut s2 = s1.clone();
+
+    println!("string_ownership2:: s1: {}", s1);
+    println!("string_ownership2:: s2: {}", s2);
+
+    // calling a function with a string param results in a MOVE operation    
+    do_stuff_with_string(s1);
+    // So this statement will result in a compile time error
+    //println!("s1: {}", s1);
+
+    // here s2 is passed as a IMMUTABLE reference (a pointer to the string)
+    // even though the value being passed is mutable. 
+    do_stuff_with_string_reference(&s2);
+    println!("string_ownership2:: s2 after function call : {}", s2);
+
+    // to pass sting to a function as a mutable reference, we need to use the mut keyword
+    update_stuff_with_string_reference(&mut s2);
+    println!("string_ownership2:: s2 after mutable function call : {}", s2);
+
+    replace_string_using_reference(&mut s2);
+    println!("string_ownership2:: s2 after mutable replacement function call : {}", s2);
+
+    // At any point in time, we can have only one mutable reference to a string and any number of immutable references 
+    // across ALL threads.
+}
+
+// function takes a string as a param
+fn do_stuff_with_string(s: String) {
+    println!("do_stuff_with_string:: s: {}", s);
+}
+
+
+// function takes a string REFERENCE as a param
+fn do_stuff_with_string_reference(s: &String) {
+    println!("do_stuff_with_string_reference:: s: {}", s);
+}
+
+// function takes a string REFERENCE as a param
+fn update_stuff_with_string_reference(s: &mut String) {
+    println!("do_stuff_with_string_reference:: s: {}", s);
+
+    // note: The dot operator auto dereferences down to the actual string value so we can use regular string methods    
+    s.insert_str(0, "[Update in progress ...]  ")
+}
+
+// function takes a string REFERENCE as a param
+fn replace_string_using_reference(s: &mut String) {
+
+    // using dereference operator to change value of the string   
+    (*s) = String::from("We have CHANGED!");    
 }
